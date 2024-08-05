6d5947521eb5fbbac0c5fa661a275679100ea6e9
stepan TeSt <stepan.prosin@mail.ru>
2024-08-06 01:32:43 +0300

    initial commit

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/NewTest.java
@@ -0,0 +1,32 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+
+@RunWith(AndroidJUnit4.class)
+public class NewTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+    @Test
+    public void useAppContext() {
+        onView(withId(R.id.text_home))
+                .check(matches(withText("This is home fragment")));
+    }
+
+}
